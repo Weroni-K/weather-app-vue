ce2bac3a134870bd7dae7472f248f92e873425d0
Weroni-K <104148980+Weroni-K@users.noreply.github.com>
2024-08-04 18:44:12 +0300

    updated App.vue

--- src/App.vue
+++ src/App.vue
@@ -22,3 +22,56 @@
     <div class="section"></div>
   </div>
 </template>
+
+<script setup>
+import { ref, computed } from 'vue'
+import axios from 'axios'
+
+const apikey = 'b09588d826e97199e406a30e962cdb60'
+
+const weatherData = ref(null)
+const suggestions = ref([])
+const city = ref('')
+
+const backgroundMapping = {
+  '01d': 'sunny-day',
+  '01n': 'sunny-night',
+  '02d': 'few-clouds-day',
+  '02n': 'few-clouds-night',
+  '03d': 'scattered-clouds-day',
+  '03n': 'scattered-clouds-night',
+  '04d': 'broken-clouds-day',
+  '04n': 'broken-clouds-night',
+  '09d': 'shower-rain-day',
+  '09n': 'shower-rain-night',
+  '10d': 'rain-day',
+  '10n': 'rain-night',
+  '11d': 'thunderstorm-day',
+  '11n': 'thunderstorm-night',
+  '13d': 'snow-day',
+  '13n': 'snow-night',
+  '50d': 'mist-day',
+  '50n': 'mist-night'
+}
+
+const currentBackgroundClass = computed(() =>
+  weatherData.value ? backgroundMapping[weatherData.value.weather[0].icon] : ''
+)
+
+const fetchCitySuggestions = async () => {
+  if (!city.value) {
+    suggestions.value = []
+    return
+  }
+  try {
+    const url = `http://api.openweathermap.org/data/2.5/find?q=${city.value}&appid=${apikey}`
+    const response = await axios.get(url)
+    suggestions.value = response.data.list.map((city) => ({
+      name: city.name,
+      country: city.sys.country
+    }))
+  } catch (error) {
+    console.error('Error fetching city suggestions:', error)
+  }
+}
+</script>
