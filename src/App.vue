4bb02db1f390ba7827707b6d97d40926d9870d99
Weronika <104148980+Weroni-K@users.noreply.github.com>
2024-09-17 20:30:20 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
-  <div :class="['container', currentBackgroundClass]">
+  <div :class="['container', currentBackgroundClass, hoverBackgroundClass]">
     <div class="header">
-      <h1>Weather app</h1>
-      <button @click="fetchCurrentLocation" class="location-button">
+      <h1>WEATHER APP</h1>
+      <button @click="fetchCurrentLocationWeather" class="location-button">
         <img class="location-icon" src="/src/assets/icons/location.svg" alt="Location Icon" />
       </button>
       <div class="search-bar">
@@ -13,13 +13,75 @@
           class="search-input"
           @input="fetchCitySuggestions"
         />
+
         <button @click="searchByCity" class="search-button">Search</button>
+        <ul v-if="suggestions.length" class="suggestions-list">
+          <li
+            v-for="(suggestion, index) in suggestions"
+            :key="index"
+            @click="selectCity(suggestion)"
+            class="suggestion-item"
+          >
+            {{ suggestion.name }}, {{ suggestion.country }}
+          </li>
+        </ul>
       </div>
     </div>
 
-    <div class="main-section"></div>
-    <div class="section"></div>
-    <div class="section"></div>
+    <div class="forecast-container">
+      <div v-if="weatherData" class="weather-now">
+        <h2>{{ weatherData.name }}, {{ weatherData.sys.country }}</h2>
+        <h3>{{ currentDate }}</h3>
+        <div class="status-box">
+          <img :src="iconUrl" alt="Weather Icon" class="weather-icon-big" />
+          <p class="status-now">{{ temperature }}°C, {{ weatherData.weather[0].description }}</p>
+        </div>
+      </div>
+
+      <div
+        class="forecast-hour-list-item"
+        v-for="(forecast, index) in hourlyForecast.slice(0, 4)"
+        :key="index"
+        @mouseover="setBackgroundClass(forecast.icon)"
+        @mouseleave="resetBackgroundClass"
+      >
+        <h3 class="time">{{ forecast.time }}</h3>
+        <div class="status-box">
+          <img :src="iconMapping[forecast.icon]" alt="Weather Icon" class="weather-icon" />
+          <p class="status-hourly">
+            <template v-if="forecast.temp_min === forecast.temp_max">
+              {{ forecast.temp_min }}°C, {{ forecast.description }}
+            </template>
+            <template v-else>
+              {{ forecast.temp_min }}°C - {{ forecast.temp_max }}°C, {{ forecast.description }}
+            </template>
+          </p>
+        </div>
+      </div>
+
+      <div class="spacer-row"></div>
+
+      <div
+        class="forecast-day-list-item"
+        v-for="(forecast, index) in dailyForecast"
+        :key="index"
+        @mouseover="setHoverBackground(forecast.icon)"
+        @mouseleave="resetHoverBackground"
+      >
+        <h2 class="date">{{ forecast.date }}</h2>
+        <div class="status-box">
+          <img :src="iconMapping[forecast.icon]" alt="Weather Icon" class="weather-icon" />
+          <p class="status-hourly">
+            <template v-if="forecast.temp_min === forecast.temp_max">
+              {{ forecast.temp_min }}°C, {{ forecast.description }}
+            </template>
+            <template v-else>
+              {{ forecast.temp_min }}°C - {{ forecast.temp_max }}°C, {{ forecast.description }}
+            </template>
+          </p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
